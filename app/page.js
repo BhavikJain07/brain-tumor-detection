1786bd99e63528608df684b266e097674e84de50
BhavikJain07 <bhavikjainhp@gmail.com>
2024-08-03 12:08:40 +0530

    init

--- app/page.js
+++ app/page.js
@@ -1,113 +1,138 @@
+"use client";
+import React, { useState, useRef } from "react";
+import { ModeToggle } from "@/components/ThemeToggle";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
 import Image from "next/image";
+import { Progress } from "@/components/ui/progress";
+import { Loader2 } from "lucide-react";
 
-export default function Home() {
+function Page() {
+  const [isLoading, setLoading] = useState(false);
+  const [file, setFile] = useState(null);
+  const handleFileChange = async (e) => {
+    setFile(URL.createObjectURL(e.target.files[0]));
+  };
+  const handlePredict = async (e) => {
+    setLoading(true);
+    setTimeout(() => {
+      setPrediction({
+        yes: 50,
+        no: 50,
+      });
+      setLoading(false);
+    }, 2000);
+  };
+
+  const handleClear = () => {
+    setFile(null);
+    setPrediction(null);
+    document.getElementById("picture").value = null;
+  };
+
+  const [prediction, setPrediction] = useState(null);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div className="max-w-screen min-h-screen flex justify-center align-middle flex-row">
+      <Card className="w-full m-8">
+        <CardHeader>
+          <div className="w-full justify-between flex flex-row">
+            <CardTitle>Brain Tumor Detection</CardTitle>
+            <ModeToggle />
+          </div>
+        </CardHeader>
+        <CardContent>
+          <div className="w-full flex flex-row justify-between">
+            <div className="w-1/2 h-full m-1">
+              <Card>
+                <CardHeader>
+                  <CardTitle>Input</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <div className="grid w-full items-center gap-1.5 my-3">
+                    <Label htmlFor="picture">Picture</Label>
+                    <Input
+                      id="picture"
+                      type="file"
+                      onChange={(e) => handleFileChange(e)}
+                      disabled={isLoading}
+                    />
+                  </div>
+                  <Separator className="my-3" />
+                  <Card className="my-3 flex justify-center align-middle p-2">
+                    <CardContent>
+                      {file && (
+                        <Image
+                          width={200}
+                          height={200}
+                          src={file}
+                          alt="Input Image"
+                        />
+                      )}
+                    </CardContent>
+                  </Card>
+                </CardContent>
+              </Card>
+            </div>
+            <div className="w-1/2 h-full m-1">
+              {isLoading ? (
+                <>
+                  <Card>
+                    <CardContent className="w-full flex flex-row justify-center align-middle m-2">
+                      <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                    </CardContent>
+                  </Card>
+                </>
+              ) : (
+                <></>
+              )}
+              {prediction && (
+                <Card>
+                  <CardHeader>
+                    <CardTitle>Predictions</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    <Label htmlFor="yesProgress">Yes</Label>
+                    <Progress id="yesProgress" value={prediction.yes} />
+                    <Label htmlFor="noProgress">No</Label>
+                    <Progress id="noProgress" value={prediction.no} />
+                  </CardContent>
+                </Card>
+              )}
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <div className="w-full flex flex-end">
+            <Button
+              variant="secondary"
+              className="m-1"
+              onClick={handleClear}
+              disabled={isLoading}
+            >
+              Clear
+            </Button>
+            <Button
+              className="m-1"
+              onClick={(e) => handlePredict(e)}
+              disabled={isLoading}
+            >
+              Predict
+            </Button>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
+
+export default Page;
